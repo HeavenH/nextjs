c323b9ee30b05c1ae8134735e5f1cc024dae862a
Gilmar Silva <gilmarheavensilva@hotmail.com>
2020-10-27 10:31:31 -0300

    add flex and initial geist template

---
+++ styles/geist.js
@@ -0,0 +1,5 @@
+export const geist = {
+    geist_page_margin: "16pt",
+    geist_page_width_with_margin: "782pt",
+    geist_gap: "16pt",
+}
---
+++ styles/main.js
@@ -0,0 +1,104 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+`;
+
+export const HeaderContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Navbar = styled.div`
+    display: flex;
+    flex: 1;
+`;
+
+export const SubmenuContainer = styled.div`
+    display: flex;
+    margin: auto;
+    width: 782pt;
+    padding: 16pt;
+    transform: translate3d(-30px, 0px, 0px);
+`;
+
+export const Submenu = styled.nav`
+    display: flex;
+`;
+
+export const MenuTab = styled.a`
+    color: black;
+    padding: 16px 12px;
+    text-decoration: none;
+    font-size: 14px;
+    font-weight: 400;
+    cursor: pointer;
+`;
+
+export const DashboardContainer = styled.div`
+    padding: 16pt;
+`;
+
+export const ProfileContainer = styled.div`
+    
+`;
+
+export const ProfileBox = styled.div`
+    margin: auto;
+    width: 782pt;
+    display: flex;
+`;
+
+export const AvatarContainer = styled.div`
+    
+`;
+
+export const ProfileAvatar = styled.img`
+    width: 100px;
+    border-radius: 50%;
+`;
+
+export const ProfileDetails = styled.div`
+    margin-left: 30px;
+`;
+
+export const ProfileName = styled.h2`
+    font-size: 2.25rem;
+    letter-spacing: -.049375rem;
+    font-weight: 600;
+`;
+
+export const Subname = styled.h6`
+`;
+
+export const CardContainer = styled.div`
+    display: flex;
+`;
+
+export const Card = styled.div`
+    padding: 1.5rem;
+    flex-basis: 30%;
+    box-shadow: 1px 1px 5px #aaaaaa;
+    border-radius: 8px;
+`;
+
+
+export const InfoTop = styled.div`
+    display: flex;
+    margin: 20px;
+    justify-content: space-between;
+`;
+
+export const InfoName = styled.div`
+    font-size: 22px;
+    color: black;
+`;
+
+export const Visit = styled.div`
+    padding: 2px 32px;
+    color: black;
+    border: 1px solid #eaeaea;
+`;
+
+export const InfoBottom = styled.div`
+    display: flex;
+`;
